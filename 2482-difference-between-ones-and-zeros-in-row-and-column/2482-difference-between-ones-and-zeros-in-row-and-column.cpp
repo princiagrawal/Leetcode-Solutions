a14dfc0ceb5f5baf503dfbc5d5bd3bc82d5f6d13
Princi Agrawal <110727015+princiagrawal@users.noreply.github.com>
2023-12-14 17:52:31 +0530

    Time: 192 ms (69.74%), Space: 116.6 MB (86.97%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -3,27 +3,20 @@ public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<int>row(n);
-        vector<int>col(m);
+        vector<int>rowOne(n),colOne(m);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                row[i]+=grid[i][j];
-                col[j]+=grid[i][j];
+                if(grid[i][j]==1){
+                    rowOne[i]++;
+                    colOne[j]++;
+                }
             }
         }
-        vector<vector<int>>diff(n,vector<int>(m));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                diff[i][j]=row[i]+col[j]-(n-row[i])-(m-col[j]);
+                grid[i][j]=2*(rowOne[i]+colOne[j])-n-m;
             }
         }
-        return diff;
+        return grid;
     }
 };
-/*
-Simply evaluate no of 1's in row and col by taking the sum of the row and col.
-Then evaluate the diff matrix.
-To evaluate 0's count, subtract sum from the size of the row or col
-so total value of diff[i][j] = row[i] + col[j] - (grid.size()- row[i]) - (grid[0].size() - col[j]) 
-above equation can be simplified as : diff[i][j] = 2*row[i] + 2*col[j] - grid.size() - grid[0].size();
-*/
